a41f43abe06622c84ecbd6ec2a731cdc95abb7a4
Ghost-Rider-gu <ghostrider.gu@gmail.com>
2017-02-22 23:52:55 +0300

    update GameMenu class

--- AlienShooter/src/main/java/GameEngine/GameMenu.java
+++ AlienShooter/src/main/java/GameEngine/GameMenu.java
@@ -2,12 +2,18 @@ package GameEngine;
 
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.GameState;
 import org.newdawn.slick.state.StateBasedGame;
 
-public class GameMenu extends BasicGameState
+public class GameMenu implements GameState
 {
+    public GameMenu(int gameMenu)
+    {
+
+    }
 
     @Override
     public int getID()
@@ -30,6 +36,126 @@ public class GameMenu extends BasicGameState
     @Override
     public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int i) throws SlickException
     {
+
+    }
+
+    @Override
+    public void enter(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
+
+    }
+
+    @Override
+    public void leave(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
+
+    }
+
+    @Override
+    public void controllerLeftPressed(int i) {
+
+    }
+
+    @Override
+    public void controllerLeftReleased(int i) {
+
+    }
+
+    @Override
+    public void controllerRightPressed(int i) {
+
+    }
+
+    @Override
+    public void controllerRightReleased(int i) {
+
+    }
+
+    @Override
+    public void controllerUpPressed(int i) {
+
+    }
+
+    @Override
+    public void controllerUpReleased(int i) {
+
+    }
+
+    @Override
+    public void controllerDownPressed(int i) {
+
+    }
+
+    @Override
+    public void controllerDownReleased(int i) {
+
+    }
+
+    @Override
+    public void controllerButtonPressed(int i, int i1) {
+
+    }
+
+    @Override
+    public void controllerButtonReleased(int i, int i1) {
+
+    }
+
+    @Override
+    public void keyPressed(int i, char c) {
+
+    }
+
+    @Override
+    public void keyReleased(int i, char c) {
+
+    }
+
+    @Override
+    public void mouseWheelMoved(int i) {
+
+    }
+
+    @Override
+    public void mouseClicked(int i, int i1, int i2, int i3) {
+
+    }
+
+    @Override
+    public void mousePressed(int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void mouseReleased(int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void mouseMoved(int i, int i1, int i2, int i3) {
+
+    }
+
+    @Override
+    public void mouseDragged(int i, int i1, int i2, int i3) {
+
+    }
+
+    @Override
+    public void setInput(Input input) {
+
+    }
+
+    @Override
+    public boolean isAcceptingInput() {
+        return false;
+    }
+
+    @Override
+    public void inputEnded() {
+
+    }
+
+    @Override
+    public void inputStarted() {
 
     }
 }
