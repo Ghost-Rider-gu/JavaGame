905fb912ec88fde8162239577da8f2a9eba3d107
Ghost-Rider-gu <ghostrider.gu@gmail.com>
2017-02-24 20:48:50 +0300

    add new game state (subtitles)

---
+++ AlienShooter/src/main/java/GameEngine/GameSubtitles.java
@@ -0,0 +1,49 @@
+package GameEngine;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class GameSubtitles extends BasicGameState
+{
+    /**
+     *  Constructor for game subtitles
+     *
+     * @param gameSubtitles
+     */
+    public GameSubtitles(int gameSubtitles)
+    {
+
+    }
+
+    /**
+     * Get state id
+     *
+     * @return int
+     */
+    @Override
+    public int getID()
+    {
+        return 3;
+    }
+
+    @Override
+    public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException
+    {
+
+    }
+
+    @Override
+    public void render(GameContainer gameContainer, StateBasedGame stateBasedGame, Graphics graphics) throws SlickException
+    {
+
+    }
+
+    @Override
+    public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int delta) throws SlickException
+    {
+
+    }
+}
