b03a3b927133f43676101c56db1be32593895dd5
Ghost-Rider-gu <ghostrider.gu@gmail.com>
2017-02-24 21:36:20 +0300

    update GameSubtitles

--- AlienShooter/src/main/java/GameEngine/GameSubtitles.java
+++ AlienShooter/src/main/java/GameEngine/GameSubtitles.java
@@ -11,7 +11,7 @@ public class GameSubtitles extends BasicGameState
     /**
      *  Constructor for game subtitles
      *
-     * @param gameSubtitles
+     * @param gameSubtitles int
      */
     public GameSubtitles(int gameSubtitles)
     {
@@ -29,18 +29,44 @@ public class GameSubtitles extends BasicGameState
         return 3;
     }
 
+    /**
+     * Prepare elements for subtitles state
+     *
+     * @param gameContainer     {@link GameContainer}
+     * @param stateBasedGame    {@link StateBasedGame}
+     *
+     * @throws SlickException   {@link SlickException}
+     */
     @Override
     public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException
     {
 
     }
 
+    /**
+     * Rendering, draw images, text or other elements
+     *
+     * @param gameContainer     {@link GameContainer}
+     * @param stateBasedGame    {@link StateBasedGame}
+     * @param graphics          {@link Graphics}
+     *
+     * @throws SlickException   {@link SlickException}
+     */
     @Override
     public void render(GameContainer gameContainer, StateBasedGame stateBasedGame, Graphics graphics) throws SlickException
     {
 
     }
 
+    /**
+     * AI, game logic
+     *
+     * @param gameContainer     {@link GameContainer}
+     * @param stateBasedGame    {@link StateBasedGame}
+     * @param delta             int
+     *
+     * @throws SlickException   {@link SlickException}
+     */
     @Override
     public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int delta) throws SlickException
     {
