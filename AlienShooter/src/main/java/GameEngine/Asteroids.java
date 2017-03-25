e348e73df1e2b3cd6ff606dc354dee44d91b2f3c
Ghost-Rider-gu <ghostrider.gu@gmail.com>
2017-03-25 23:00:46 +0300

    add Asteroids class

---
+++ AlienShooter/src/main/java/GameEngine/Asteroids.java
@@ -0,0 +1,14 @@
+package GameEngine;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
+
+/**
+ *
+ */
+public class Asteroids
+{
+    // TODO: behavior of asteroids
+}
