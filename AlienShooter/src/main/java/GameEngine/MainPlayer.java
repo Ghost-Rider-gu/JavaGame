6fbc134619e4d9ebbd379313c6bb3bf7874ca714
Ghost-Rider-gu <ghostrider.gu@gmail.com>
2017-03-25 23:00:25 +0300

    add MainPlayer class

---
+++ AlienShooter/src/main/java/GameEngine/MainPlayer.java
@@ -0,0 +1,14 @@
+package GameEngine;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
+
+/**
+ *
+ */
+public class MainPlayer
+{
+    // TODO: behavior for our player
+}
